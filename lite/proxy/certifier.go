6c1cc321a1a04500edbc848baf2b7bff8a5bc479
HaoyangLiu <lhy.0318@163.com>
2018-07-05 15:06:39 +0800

    Add new method for creating certifier

--- lite/proxy/certifier.go
+++ lite/proxy/certifier.go
@@ -4,6 +4,8 @@ import (
 	"github.com/tendermint/tendermint/lite"
 	certclient "github.com/tendermint/tendermint/lite/client"
 	"github.com/tendermint/tendermint/lite/files"
+	"errors"
+	liteErr "github.com/tendermint/tendermint/lite/errors"
 )
 
 func GetCertifier(chainID, rootDir, nodeAddr string) (*lite.InquiringCertifier, error) {
@@ -22,6 +24,32 @@ func GetCertifier(chainID, rootDir, nodeAddr string) (*lite.InquiringCertifier, 
 	if certerr.IsCommitNotFoundErr(err) {
 		return nil, errors.New("Please run init first to establish a root of trust")
 	}*/
+	if err != nil {
+		return nil, err
+	}
+
+	cert, err := lite.NewInquiringCertifier(chainID, fc, trust, source)
+	if err != nil {
+		return nil, err
+	}
+
+	return cert, nil
+}
+
+func GetCertifierWithHeight(chainID, rootDir, nodeAddr string, height int64) (*lite.InquiringCertifier, error) {
+	trust := lite.NewCacheProvider(
+		lite.NewMemStoreProvider(),
+		files.NewProvider(rootDir),
+	)
+
+	source := certclient.NewHTTPProvider(nodeAddr)
+	//Load trusted commit from trust store
+	fc, err := trust.LatestCommit()
+	if !liteErr.IsCommitNotFoundErr(err) {
+		return nil, errors.New(err.Error())
+	}
+	//If trust store is empty, get the commit at the specific height as the initial trusted commit
+	fc, err = source.GetByHeight(height)
 	if err != nil {
 		return nil, err
 	}
