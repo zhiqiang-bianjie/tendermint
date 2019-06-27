4f5acc744de7b4672cebf760dc13564e6888c4f5
chengwenxi <vincent.ch.cn@gmail.com>
2019-06-27 10:24:11 +0800

    Only log on maxAttemptsToDial+1

--- p2p/pex/pex_reactor.go
+++ p2p/pex/pex_reactor.go
@@ -472,7 +472,7 @@ func (r *PEXReactor) dialPeer(addr *p2p.NetAddress) {
 
 	if attempts > maxAttemptsToDial {
 		// Do not log the message if the addr gets readded.
-		if attempts+1 == maxAttemptsToDial {
+		if attempts == maxAttemptsToDial+1 {
 			r.Logger.Info("Reached max attempts to dial", "addr", addr, "attempts", attempts)
 			r.attemptsToDial.Store(addr.DialString(), _attemptsToDial{attempts + 1, time.Now()})
 		}
